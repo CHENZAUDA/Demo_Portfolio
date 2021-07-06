3e5de194f8a8e35b6b6881edc780091060fa3434
CHEN ZAUDA <czauda@gmail.com>
2021-07-06 17:06:02 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import RoutApp from "./Components/Features/Common/RoutApp";
-import './App.css'
+import './App.css';
+import ChatBox from "./Components/Features/ChatBox/ChatBox";
 function App(){
-  return <RoutApp/>
+  return (
+    <div>
+      <ChatBox />
+      <RoutApp/>
+      
+      </div>
+  ) 
 }
 export default App;
