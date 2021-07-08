c0b32c82046896edf317813dcfcc1be9d9d23d8d
CHEN ZAUDA <czauda@gmail.com>
2021-07-08 09:35:46 +0300

    chatboxdiv

--- src/Components/Features/Header/Header.jsx
+++ src/Components/Features/Header/Header.jsx
@@ -22,7 +22,7 @@ const HeaderLink = styled.div`
         <HeaderLink>
             <p><a href="https://tech-career-alternative.firebaseapp.com/">
                 <img src="img/techLogo.jpg" width="100" height="80px" /></a>
-                <img src="/protfolio/img/indi.jpg" width="100" height="80px" />
+                <img src="/img/indi.jpg" width="100" height="80px" />
 
             </p>
             <p style={{marginTop:"45px"}}>my email: czauda@gmail.com</p>
